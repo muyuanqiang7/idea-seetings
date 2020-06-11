5a3a3027ff524d03e4c92a8e618bc4f91a201532
muyuanqiang <muyuanqiang7@gmail.com>
2020-06-11 15:34:44 +0800

    IU-2020.1.1 <muyuanqiang@muyuanqiangsMacBook-Pro.local Create baseRefactoring.xml, markdown.xml, p3c.xml, IntelliLang.xml, ide.general.xml, ide-features-trainer.xml, CodeGlance.xml, git_toolbox_2.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, lombok-plugin.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, _@user_One Dark vivid italic.icls, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, scala_config.xml, _@user_Darcula.icls, path.macros.xml, databaseDrivers.xml, mavenVersion.xml, AnnotationType.java, stringManipulation.xml, _@user_Dark purple.icls, Default.xml, scala.xml, _@user_Material Palenight.icls, Class.java, jdk.table.xml, mavenRunHelper.xml, presentation-assistant.xml, laf.xml, _@user_Atom One Dark.icls, vcs.xml, keymap.xml, editor.xml, applicationLibraries.xml, debugger.xml, a-file-icons.xml, sonarlint.xml, updates.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author [${USER}]
+ * @date: [${DATE} ${TIME}]
+ * @version [1.0.0]
+ * @description []
+ */
+public @interface ${NAME} {
+}
